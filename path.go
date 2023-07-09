cc39ebcfce728b39a11e3a982652314023c8199c
Jumpaku <Jumpaku@users.noreply.github.com>
2023-07-09 08:40:56 +0000

    docs(jsonvalue): add comments

--- path.go
+++ path.go
@@ -7,48 +7,64 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+// Key represents a key for a member in a JSON object or an index for an element in a JSON array.
 type Key string
 
-func (e Key) String() string {
-	return string(e)
+// KeyInt creates Key from JOSN array index.
+func KeyInt(index int) Key {
+	return Key(strconv.FormatInt(int64(index), 10))
 }
-func (e Key) Int() int {
-	elm, err := strconv.ParseInt(e.String(), 10, 64)
-	assert.State(err == nil, "%v cannot be parsed to int: %w", e, err)
+
+// String returns key for JSON object in string.
+func (k Key) String() string {
+	return string(k)
+}
+
+// String returns index for JSON array in int.
+func (k Key) Int() int {
+	elm, err := strconv.ParseInt(k.String(), 10, 64)
+	assert.State(err == nil, "%v cannot be parsed to int: %w", k, err)
 
 	return int(elm)
 }
 
-func KeyInt(i int) Key {
-	return Key(strconv.FormatInt(int64(i), 10))
-}
-
+// Path represents a sequence of the Keys.
+// An empty path Path{} represents a root JSON value.
 type Path []Key
 
-func (k Path) Equals(other Path) bool {
-	return slices.Equal(k, other)
-}
-func (k Path) Len() int {
-	return len(k)
-}
-func (k Path) Append(key Key) Path {
-	return Path(append(append([]Key{}, k...), key))
+// Equals returns true if two Paths contain the same keys in the same order; otherwise false.
+func (p Path) Equals(other Path) bool {
+	return slices.Equal(p, other)
 }
 
-func (k Path) Slice(begin int, endExclusive int) Path {
-	assert.Params(0 <= begin && begin <= k.Len(), "begin %v must be in [0, %d]", begin, k.Len())
-	assert.Params(0 <= endExclusive && endExclusive <= k.Len(), "endExclusive %v must be in [0, %d]", endExclusive, k.Len())
+// Len returns the number of keys.
+func (p Path) Len() int {
+	return len(p)
+}
+
+// Append creates a new Path appended the key to the end of the original Path.
+func (p Path) Append(key Key) Path {
+	return Path(append(append([]Key{}, p...), key))
+}
+
+// Slice creates a new Path sliced from begin to and of the original Path.
+func (p Path) Slice(begin int, endExclusive int) Path {
+	assert.Params(0 <= begin && begin <= p.Len(), "begin %v must be in [0, %d]", begin, p.Len())
+	assert.Params(0 <= endExclusive && endExclusive <= p.Len(), "endExclusive %v must be in [0, %d]", endExclusive, p.Len())
 	assert.Params(begin <= endExclusive, "begin %v and endExclusive %v must be begin <= endExclusive", begin, endExclusive)
 
-	return Path(append([]Key{}, k[begin:endExclusive]...))
+	return Path(append([]Key{}, p[begin:endExclusive]...))
 }
 
-func (k Path) Get(index int) Key {
-	assert.Params(0 <= index && index < len(k), "index must be in [0, %d)", len(k))
+// Get returns a Key at the index.
+func (p Path) Get(index int) Key {
+	assert.Params(0 <= index && index < len(p), "index must be in [0, %d)", len(p))
 
-	return k[index]
+	return p[index]
 }
 
+// Walk traverses a JSON value v and calls the visitor function for each the JSON values included in v.
+// If a call of visitor returned an error, Walk immediately returns with the error.
 func Walk(v Value, visitor func(path Path, val Value) error) error {
 	return walkImpl(Path{}, v, visitor)
 }
@@ -76,6 +92,8 @@ func walkImpl(parentKey Path, val Value, walkFunc func(key Path, val Value) erro
 	return nil
 }
 
+// Find finds the JSON value specified by the Path in a JSON value v.
+// If the JSON value associated with the Path exists, the found JSON value and true are returned; otherwise nil and false are returned.
 func Find(v Value, path Path) (Value, bool) {
 	var found Value
 	_ = Walk(v, func(p Path, val Value) error {
